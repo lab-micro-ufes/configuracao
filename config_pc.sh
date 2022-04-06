ebdf76c5cba4fb875e42a2af42e19590135b3149
tpnascimento <thaispedruzzi@gmail.com>
2022-04-06 17:29:06 -0300

    first commit

---
+++ config_pc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+# Clona repositorio com os roteiros para o desktop 
+cd /home/professor/Desktop/
+git clone https://github.com/tpnascimento/labmicro.git
+
+# Instala Dosbox
+sudo apt install dosbox
+
+# Instala visual studio code
+sudo snap install --classic code
