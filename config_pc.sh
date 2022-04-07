d619e0794bdf2fc06e111b3cfe8f609cf4bbe30d
Thais Pedruzzi do Nascimento <12402398+tpnascimento@users.noreply.github.com>
2022-04-07 14:02:34 -0300

    Update config_pc.sh

--- config_pc.sh
+++ config_pc.sh
@@ -13,4 +13,4 @@ sudo snap install --classic code
 sudo adduser aluno 
 
 # Copia a pasta dos roteiros para o usuario "aluno"
-scp -r ../Roteiros aluno@$(hostname):~
+sudo cp -r ../Roteiros /home/aluno/
